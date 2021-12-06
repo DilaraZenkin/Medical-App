71796d36a898d32723eaf9557741bc987e9c63e0
Greg Barker <barkerg252@gmail.com>
2021-12-06 06:06:33 -0500

    capstone updated

---
+++ backend/src/main/java/com/techelevator/controller/DoctorAvailabilityController.java
@@ -0,0 +1,15 @@
+package com.techelevator.controller;
+
+//public class DoctorAvailabilityController {
+//    public DoctorAvailability getDoctorAvailabilityForMonth(Long doctorId, int month, int year);
+//
+//    public DoctorAvailability setRegularAvailability(DoctorAvailability regularAvailability);
+//
+//    public DoctorAvailability setSpecificAvailability(DoctorAvailability specificAvailability);
+//
+//    public DoctorAvailability updateRegularAvailability(DoctorAvailability regularAvailability);
+//
+//    public DoctorAvailability updateSpecificAvailability(DoctorAvailability specificAvailability);
+//
+//    public void deleteSpecificAvailability(DoctorAvailability specificAvailability);
+//}
---
+++ backend/src/main/java/com/techelevator/dao/DoctorAvailabilityDao.java
@@ -0,0 +1,11 @@
+//package com.techelevator.dao;
+//
+//import com.techelevator.model.DoctorAvailability;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//public interface DoctorAvailabilityDao {
+//    @RequestMapping(path = "/availability", method = RequestMethod.GET)
+//    DoctorAvailability getDoctorAvailability(long doctorAvailabilityId);
+//}
