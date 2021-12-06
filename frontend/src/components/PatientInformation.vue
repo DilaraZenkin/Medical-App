8248b491d27ba4fd06a30a3031ed3f31906bc059
timothy.ziegler <timothy.ziegler90@gmail.com>
2021-12-06 17:40:39 -0500

    started to implement patient view

--- frontend/src/components/PatientInformation.vue
+++ frontend/src/components/PatientInformation.vue
@@ -1,26 +1,33 @@
 <template>
-  <tr class="patient-info">
+    <tr class="patient-info">
       <td id = "patient-id">{{patient.patientId}}</td>
       <td id = "patient-first-name">{{patient.firstName}}</td>
       <td id = "patient-last-name">{{patient.lastName}}</td>
       <td id = "patient-date-of-birth">{{patient.dateOfBirth}}</td>
       <td id = "patient-address">{{patient.patientAddress}}</td>
+    </tr>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            patient: {
-                patientId: '',
-                firstName: '',
-                lastName: '',
-                dateOfBirth: '',
-                patientAddress: '',
-            }
-        }
-    }
-}
+// import PatientService from '../services/PatientService';
+// export default {
+//     data() {
+//         return {
+//             patient: {
+//                 patientId: '',
+//                 firstName: '',
+//                 lastName: '',
+//                 dateOfBirth: '',
+//                 patientAddress: '',
+//             }
+//         };
+//     },
+//     created() {
+//         PatientService.getPatient().then(response => {
+//             this.patient = response.data;
+//         })
+//     }
+// }
 </script>
 
 <style>
---
+++ frontend/src/views/PatientHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <patient-information />
+</template>
+
+<script>
+import PatientInformation from '../components/PatientInformation.vue'
+export default {
+  components: { PatientInformation },
+
+}
+</script>
+
+<style>
+
+</style>
