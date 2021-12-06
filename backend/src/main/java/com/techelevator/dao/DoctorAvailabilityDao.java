e6012a22d5670ebd8323b168dc69a7cc41cf389d
Greg Barker <barkerg252@gmail.com>
2021-12-06 07:46:39 -0500

    capstone. backend on availability ready for review

--- backend/src/main/java/com/techelevator/dao/DoctorAvailabilityDao.java
+++ backend/src/main/java/com/techelevator/dao/DoctorAvailabilityDao.java
@@ -1,11 +1,10 @@
-//package com.techelevator.dao;
-//
-//import com.techelevator.model.DoctorAvailability;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//public interface DoctorAvailabilityDao {
-//    @RequestMapping(path = "/availability", method = RequestMethod.GET)
-//    DoctorAvailability getDoctorAvailability(long doctorAvailabilityId);
-//}
+package com.techelevator.dao;
+
+import com.techelevator.model.DoctorAvailability;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public interface DoctorAvailabilityDAO {
+    DoctorAvailability getDoctorAvailability(long doctorAvailabilityId);
+}
