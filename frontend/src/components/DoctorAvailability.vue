ebf1b531f1571270cc5d91c768b3ec11d6962ab6
timothy.ziegler <timothy.ziegler90@gmail.com>
2021-12-15 12:30:38 -0500

    work progress of update availability

--- frontend/src/components/DoctorAvailability.vue
+++ frontend/src/components/DoctorAvailability.vue
@@ -15,10 +15,72 @@
                 </div>
                 <div class="text--primary">
                  To: {{availability.endTime}}
-                  
                 </div>
+                <v-card-text>
+                  Available: {{availability.available == true ? 'Yes' : 'No'}}
+                </v-card-text>  
+                   <v-btn class="update-availability-btn" @:click="updateForm = true; populateCurrent()">Change Availability</v-btn>
               </v-card-text>
-            </v-card>
+              <div class="update-availability" v-show="updateForm">
+                <v-form>
+                  <!-- <v-chip-group
+                      v-model="selection"
+                      active-class="deep-purple accent-4 white--text"
+                      column
+                    >
+                      <v-chip>5:30PM</v-chip>
+                      <v-chip>7:30PM</v-chip>
+                      <v-chip>8:00PM</v-chip>
+                      <v-chip>9:00PM</v-chip>
+                    </v-chip-group> -->
+                  <v-chip-group
+                    id="start"
+                    prepend-icon=""
+                    class="updateAvailability"
+                    label="Start Time"
+                    v-model="updatedAvailability.startTime"
+                    active-class="deep-purple accent-4 white--text"
+                    >
+                    Start Time:
+                    <v-spacer />
+                      <v-chip>8:00AM</v-chip>
+                      <v-chip>8:30AM</v-chip>
+                      <v-chip>9:00AM</v-chip>
+                      <v-chip>9:30AM</v-chip>
+                      <v-chip>10:00AM</v-chip>
+                      <v-chip>10:30AM</v-chip>
+                      <v-chip>11:00AM</v-chip>
+                    </v-chip-group>
+                    <v-chip-group
+                    id="end"
+                    prepend-icon=""
+                    class="updateAvailability"
+                    label="End Time"
+                    v-model="updatedAvailability.endTime"
+                    active-class="deep-purple accent-4 white--text"
+                    >
+                    End Time:
+                    <v-spacer />
+                      <v-chip>1:00PM</v-chip>
+                      <v-chip>1:30PM</v-chip>
+                      <v-chip>2:00PM</v-chip>
+                      <v-chip>2:30pm</v-chip>
+                      <v-chip>3:00PM</v-chip>
+                      <v-chip>3:30PM</v-chip>
+                      <v-chip>4:00PM</v-chip>
+                    </v-chip-group>
+                    <v-checkbox
+                    id="available"
+                    prepend-icon=""
+                    class="updateAvailability"
+                    label="Available?"
+                    v-model="updatedAvailability.available"
+                    ></v-checkbox>
+                    <v-btn type="submit" @submit.prevent="updateAvailability()">Update Availability</v-btn>
+                    <v-btn type="reset" @:reset.prevent="cancelUpdate()">Cancel Update</v-btn>
+                </v-form>
+              </div>  
+            </v-card>  
           </v-flex>
         </v-layout>
       </v-container>
@@ -48,7 +110,16 @@ export default {
     enable: false,
     patient: {},
     doctor: {},
-    availabilities: []
+    updateForm: false,
+    availabilities: [],
+    updatedAvailability: {
+      availabilityId: '',
+      doctorId: '',
+      dayOfWeek: '',
+      startTime: '',
+      endTime: '',
+      available: '',
+    },
   }),
   created() {
       AvailabilityService.getAvailability(this.$store.state.user.id).then(
@@ -61,8 +132,23 @@ export default {
     resetForm() {
       this.showForm = false;
     },
-    updatePatient() {},
+    updateAvailability() {
+      AvailabilityService.updateAvailability(this.updatedAvailability);
+    },
+    populateCurrent() {
+      this.updatedAvailability.availabilityId = this.avability.availabilityId;
+      this.updatedAvailability.doctorId = this.$store.state.user.id;
+      this.updatedAvailability.dayOfWeek = this.avability.dayOfWeek;
+      this.updatedAvailability.startTime = this.avability.startTime;
+      this.updatedAvailability.endTime = this.avability.endTime;
+      this.updatedAvailability.available = this.avability.available;
+    },
+    cancelUpdate() {
+      this.updateForm = false;
+      this.populateCurrent();
+    }
   },
+  
 };
 </script>
 
